7113ceb2ced4b4fcf3fa5ef8cf4abc62122f1f75
Alireza Soleymani <alirezasoleymani1377@gmail.com>
2020-12-19 19:28:27 +0330

    Question 2 added

--- Task 2/Question 2/Code/Q2.c
+++ Task 2/Question 2/Code/Q2.c
@@ -1,13 +1,43 @@
 
 #include <mega16.h>
+#include <delay.h>
 
+int kossher;
+int SevenSegNumber[10] = {0b00111111 , 0b00000110 , 0b01011011 , 0b01001111 , 0b01100110 , 0b01101101 , 0b01111101 , 0b00000111 ,0b01111111 ,0b01101111};
 
-
+int Timer0OverFlowCounter =0;
+int Timer1OverFlowCounter =0;
+int SevenSegCounter[4] = {0,0,0,0};
+int i =3;
+int lastSevenSeg=3;
 
 // Timer 0 overflow interrupt service routine
 interrupt [TIM0_OVF] void timer0_ovf_isr(void)
 {
 
+
+   ++Timer0OverFlowCounter;
+    if(Timer0OverFlowCounter ==2){
+           kossher = PORTC ^ 0b11111111;
+           PORTC= kossher ^ 0b11111111;
+           PORTD = SevenSegNumber[SevenSegCounter[lastSevenSeg]];
+           kossher = kossher >>1;
+           if(kossher ==0){
+           kossher = 8;
+           }
+           kossher = kossher ^ 0b11111111;
+           PORTC = kossher;
+           kossher = kossher ^ 0b11111111;
+
+              Timer0OverFlowCounter=0;
+             lastSevenSeg--;
+             if(lastSevenSeg == -1 ){
+             lastSevenSeg = 3;
+             }
+    }
+
+
+       TCNT0=0x00;
 
 }
 
@@ -15,7 +45,31 @@ interrupt [TIM0_OVF] void timer0_ovf_isr(void)
 interrupt [TIM1_OVF] void timer1_ovf_isr(void)
 {
 
+    ++Timer1OverFlowCounter;
+    if(Timer1OverFlowCounter ==500){
 
+         SevenSegCounter[3]++;
+
+         while (i>=0){
+
+            if( SevenSegCounter[i] ==10){
+
+            SevenSegCounter[i-1]++;
+            SevenSegCounter[i]=0;
+
+            }
+             i--;
+         }
+
+
+                   i=3;
+             Timer1OverFlowCounter=0;
+
+    }
+
+
+        TCNT1H=0xFF;
+        TCNT1L=0xF0;
 
 }
 
@@ -29,7 +83,8 @@ void main(void)
 DDRD=0b11111111;
 PORTD=0b00000000;
 DDRC=0b11111111;
-PORTC=0b00000000;
+PORTC=0b11101111;
+ // 0b00001000 = 8 decimal
 
 
 
@@ -45,7 +100,7 @@ TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (1<<TO
 // Mode: Normal top=0xFF
 // OC0 output: Disconnected
 // Timer Period: 0.032 ms
-TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (0<<CS01) | (1<<CS00);
+TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (1<<CS02) | (0<<CS01) | (1<<CS00);
 TCNT0=0x00;
 OCR0=0x00;
 
@@ -65,15 +120,10 @@ OCR0=0x00;
 // Compare A Match Interrupt: Off
 // Compare B Match Interrupt: Off
 TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
-TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (1<<CS10);
-TCNT1H=0x00;
-TCNT1L=0x00;
-ICR1H=0x00;
-ICR1L=0x00;
-OCR1AH=0x00;
-OCR1AL=0x00;
-OCR1BH=0x00;
-OCR1BL=0x00;
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (1<<CS12) | (0<<CS11) | (1<<CS10);
+TCNT1H=0xFF;
+TCNT1L=0xF0;
+
 
 
 // External Interrupt(s) initialization
@@ -83,8 +133,12 @@ OCR1BL=0x00;
 MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
 MCUCSR=(0<<ISC2);
 
+#asm("sei")
 while (1)
       {
+
+
+
 
       }
 }
